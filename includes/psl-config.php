f344f28527c792edfbfd2ba7ea5eaf32d4c6f551
kandoudou <jeremie.kandoudaeff@gmail.com>
2015-05-20 20:05:34 +0200

    creation de compte utilisateur

---
+++ includes/psl-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Voici les détails de connexion à la base de données
+ */  
+define("HOST", "localhost");     // L’hébergeur où vous voulez vous connecter.
+define("USER", "japonais_user");    // Le nom d’utilisateur de la base de données.
+define("PASSWORD", "E5eBs4NgR6CPMvCxbhrgrjsn");    // Le mot de passe de la base de données. 
+define("DATABASE", "japonais_login");    // Le nom de la base de données.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // SEULEMENT DANS LE CADRE DE CE GUIDE!!!!
